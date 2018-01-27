4070bce1f2829ee04899a1312760fb9697cd1d7b
TheJP <jp_55555@hotmail.com>
2018-01-27 05:21:47 +0100

    Started to implement networking code

---
+++ NetworkBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlobalGameJam2018Networking
+{
+    public abstract class NetworkBase
+    {
+        public const int DefaultPort = 505;
+
+        protected readonly Action<Action> invoke;
+
+        public abstract event Action<string> ReceivedMessage;
+
+        public NetworkBase(Action<Action> invoke)
+        {
+            this.invoke = invoke;
+        }
+
+        public abstract void SendMessage(string message);
+    }
+}
---
+++ PipesNetwork.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace GlobalGameJam2018Networking
+{
+    public class PipesNetwork : NetworkBase
+    {
+        public override event Action<string> ReceivedMessage;
+
+        private TcpListener tcpListener;
+
+        /// <param name="invoke">Action that invokes given actions on the main unity thread.</param>
+        public PipesNetwork(Action<Action> invoke) : base(invoke) { }
+
+        public void Start(string username, int port = DefaultPort)
+        {
+            if (tcpListener != null) { throw new InvalidOperationException($"Can't start {nameof(PipesNetwork)} multiple times"); }
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(AcceptClient, null);
+        }
+
+        private void AcceptClient(IAsyncResult result)
+        {
+            new Thread(new ThreadStart(() =>
+            {
+                var tcpClient = tcpListener.EndAcceptTcpClient(result);
+                var serializer = new JsonSerializer();
+                using (var reader = new StreamReader(tcpClient.GetStream()))
+                using (var jsonTextReader = new JsonTextReader(reader))
+                {
+                    jsonTextReader.SupportMultipleContent = true;
+                    while (jsonTextReader.Read())
+                    {
+                        serializer.Deserialize<object>(jsonTextReader);
+                    }
+                }
+            })).Start();
+        }
+
+        public override void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
